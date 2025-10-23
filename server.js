ce00fa3bd8a6279e6047fdce14367bba1cd73a74
Tarun Kumar <yadavatarun18@gmail.com>
2025-10-23 10:58:30 +0530

    fix

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get("/auth/google", passport.authenticate("google", {
   scope: ["profile", "email"],
 }));
 
-app.get("/auth/google/otunar/", passport.authenticate("google", {
+app.get("/auth/google/otunar", passport.authenticate("google", {
   successRedirect: process.env.APPLICATION_URL,
   failureRedirect: process.env.APPLICATION_URL + "/login?error=Invalid credentials",
 }));
